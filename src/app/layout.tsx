fa6f1c9810137f4e5b8a7d1c2b83664cc9797b3a
听风 <3336016933@qq.com>
2025-02-12 14:16:42 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
-import Script from 'next/script'
 import "./globals.css";
 import style9 from "style9";
+import AnalyticsScript from './AnalyticsScript';
 
 const styles = style9.create({
   body: {
@@ -9,37 +9,27 @@ const styles = style9.create({
     padding: 0,
     backgroundColor: "var(--bg-color)",
   },
-})
+});
 
 export const metadata: Metadata = {
   title: "unify-z's Home Page",
   description: "A home page for unify-z",
   icons: '/favicon.ico',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const handleLAOnLoad = () => {
-      (window as any).LA.init({ id: "3LEhgRw88R4WHpfa", ck: "3LEhgRw88R4WHpfa" });
-  };
-
   return (
     <html lang="Zh-cn">
       <head>
-        <Script
-          strategy="afterInteractive"
-          src="//sdk.51.la/js-sdk-pro.min.js"
-          id="LA_COLLECT"
-          charSet="UTF-8"
-          onLoad={handleLAOnLoad}
-        />
+        <AnalyticsScript />
       </head>
       <body className={styles("body")}>
         {children}
       </body>
     </html>
-  )
+  );
 }
